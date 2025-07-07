17ba4e4dbef450ecb0acf60929e2b9da6a9c16fb
Dmitriy Ryzhov <superdiman_1994@mail.ru>
2025-07-07 17:01:39 +0800

    Заглушка тестов

--- test/tic_tac_toe_test.dart
+++ test/tic_tac_toe_test.dart
@@ -1,8 +1,8 @@
-import 'package:tic_tac_toe/tic_tac_toe.dart';
-import 'package:test/test.dart';
+// import 'package:tic_tac_toe/tic_tac_toe.dart';
+// import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
